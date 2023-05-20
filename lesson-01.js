571f7fbdf5a71222f8ca50ddecbe3a142666ae6a
Volodymyr Ionaytis <blessfire91@gmail.com>
2023-05-20 09:59:12 +0200

    task2

--- lesson-01.js
+++ lesson-01.js
@@ -1,16 +1,34 @@
-//Використовуя функцію if...else,
-//напишіть код, який буде питати:
-//"Яка офіційна назва JavaScript?"
-//Якщо користувач вводить "ECMAScript",
-//то показати через alert: "Вірно!"
-//в противному випадку відобразити:"Не знаєте? ECMAScript!"
+// //Використовуя функцію if...else,
+// //напишіть код, який буде питати:
+// //"Яка офіційна назва JavaScript?"
+// //Якщо користувач вводить "ECMAScript",
+// //то показати через alert: "Вірно!"
+// //в противному випадку відобразити:"Не знаєте? ECMAScript!"
 
-const jsName = prompt("Яка офіційна назва JavaScript?");
+// const jsName = prompt("Яка офіційна назва JavaScript?");
 
-// if (jsName === "ECMAScript") {
-//   alert("Вірно!");
-// } else {
-//   alert("Не знаєте? ECMAScript!");
-// }
+// // if (jsName === "ECMAScript") {
+// //   alert("Вірно!");
+// // } else {
+// //   alert("Не знаєте? ECMAScript!");
+// // }
 
-alert(jsName === "ECMAScript" ? "Вірно!" : "Не знаєте? ECMAScript!");
+// alert(jsName === "ECMAScript" ? "Вірно!" : "Не знаєте? ECMAScript!");
+
+// *******======// *******======// *******======// *******======
+// Напишіть программу, яка отримує від користувача
+// число (кількість хвилин) и виводить у консоль
+// рядок у форматі годин та хвилин
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
+// 70 === 01:10
+
+// const number = prompt("Введіть число!");
+// let hours = Math.floor(number / 60);
+// hours = String(hours).padStart(2, 0);
+// let minutes = number % 60;
+// minutes = String(minutes).padStart(2, 0);
+
+// console.log(`${hours}:${minutes}`);
+
+
+
