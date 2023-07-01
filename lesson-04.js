f6f39387db10e25b2edb4f5bc090b521946993a9
Viacheslav <gabon.qwa@gmail.com>
2023-07-01 11:21:13 +0300

    task02

--- lesson-04.js
+++ lesson-04.js
@@ -49,4 +49,37 @@
 //     lastItemElement.remove();
 //   }
 // }
+// ******************************task02
+// Створити невелику гру
+// Спочатку на екрані з'являється якась фігура рандомного коліру в рандомному місті
+// Натискаючі на фігуру, вона змінює свою форму, колір, місце розташування
 
+// const forms = [
+//   "width: 100px; height: 100px; border-width: 1px;",
+//   "width: 100px; height: 100px; border-radius: 50%;",
+//   "width: 150px; height: 100px; border-width: 1px;",
+//   "width: 200px; height: 100px; border-radius: 100px / 50px; ",
+//   "width: 150px; height: 100px; transform: skew(20deg)",
+// ];
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+
+// function randomither(max) {
+//   return Math.floor(Math.random() * max);
+// }
+
+// const elementPlay = document.createElement('div');
+// changePosition();
+// document.body.append(elementPlay);
+
+// elementPlay.addEventListener('click', changePosition);
+
+// function changePosition() {
+//   elementPlay.style.cssText = forms[randomither(forms.length)];
+//   elementPlay.style.backgroundColor = getRandomHexColor();
+//   elementPlay.style.position = "absolute";
+//   elementPlay.style.top = `${randomither(100)}%`;
+//   elementPlay.style.left = `${randomither(100)}%`;
+// }
