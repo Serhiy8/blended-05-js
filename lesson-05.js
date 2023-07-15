7b3e8b5694a00ee066d3b1f9007d11616f8a65e9
DariusBlac <legionnsspace@gmail.com>
2023-07-15 10:12:38 +0200

    1

--- lesson-05.js
+++ lesson-05.js
@@ -17,3 +17,51 @@
 //     const markup = `Ви вибрали ${productName} за ${productPrice}`;
 //     containerEl.innerHTML = markup;
 // }
+
+// TASK 02
+//TODO:==============================================
+/*
+Завдання 8
+При натисканні на кожну з кнопок підсумовуються значення з data-атрибутів.
+За натисканням на кнопку "Вивести результат" виводиться сума значення, а також статистика з
+інформацією про те, яка кнопка була натиснута скільки разів.
+*/
+
+// const divEl = document.querySelector('.statList');
+// const btnEl = document.querySelector('#resultButton');
+// const textEl = document.querySelector('#resultSection');
+
+// divEl.addEventListener('click', onBtnClick);
+// btnEl.addEventListener('click', onResult);
+
+// let sum = 0;
+
+// let objBtn = {};
+
+// function onBtnClick(event) {
+//     if (event.target.nodeName !== 'BUTTON') return;
+//     const number = Number(event.target.dataset.number);
+//     sum += number;
+
+//     const btnName = event.target.textContent;
+//     if (!objBtn[btnName]) {
+//         objBtn[btnName] = 1;
+//     } else {
+//         objBtn[btnName] += 1;
+//     }
+// };
+
+// function onResult() {
+//     let markup = `Загальна сума ${sum}`;
+//     for (const key in objBtn) {
+//         if (Object.hasOwnProperty.call(objBtn, key)) {
+//            markup += `<br>${key} натиснута ${objBtn[key]} разів`         
+//         }
+//     }
+//     textEl.innerHTML = markup;
+//     sum = 0;
+//     objBtn = {};
+// }
+
+
+
